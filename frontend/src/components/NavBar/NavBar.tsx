2cadd2f51a6cbc74f1ae5421407bcc767fb10e6d
Lilit Yepremyan <lilith.yepremyan@gmail.com>
2026-02-25 15:12:46 +0400

    reusable navbar

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -2,9 +2,12 @@ import { NavLink } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { Tooltip } from "react-tooltip";
 import styles from "./NavBar.module.scss";
+import { useState } from "react";
+import { FiMenu, FiX } from "react-icons/fi";
 
 const NavBar = () => {
   const { t, i18n } = useTranslation();
+  const [isOpen, setIsOpen] = useState(false);
 
   const links = [
     { to: "/", label: t("booking") },
@@ -21,28 +24,42 @@ const NavBar = () => {
     i18n.changeLanguage(lng);
   };
 
+  const toggleMenu = () => {
+    console.log("Toggling menu");
+    setIsOpen((prev) => !prev);
+  };
+
   return (
     <nav className={styles.navbar}>
-      <div className={styles.linksContainer}>
+      <div
+        className={`${styles.burger} ${isOpen ? styles.active : ""}`}
+        onClick={toggleMenu}
+      >
+        <FiMenu className={styles.iconMenu} size={26} />
+        <FiX className={styles.iconClose} size={26} />
+      </div>
+
+      <div
+        className={`${styles.linksContainer} ${isOpen ? styles.mobileOpen : ""}`}
+      >
         {links.map((link) => {
           return (
             <>
               <Tooltip
                 id={link.to}
-                key={link.to}
                 content={link.label}
                 className={styles.tooltip}
               />
+
               <NavLink
-                data-tooltip-id={link.to}
-                key={link.label}
+                key={link.to}
                 to={link.to}
+                onClick={() => setIsOpen(false)}
                 className={({ isActive }) =>
                   `${styles.link} ${isActive ? styles.active : ""}`
                 }
               >
                 {link.label}
-                <span className={styles.underline} />
               </NavLink>
             </>
           );
