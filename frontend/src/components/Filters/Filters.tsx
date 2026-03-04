da60d5c50e8b2b6dcbe38b268f996230beaab3c2
Lilit Yepremyan <lilith.yepremyan@gmail.com>
2026-03-04 14:18:02 +0400

    add datepicker

--- frontend/src/components/Filters/Filters.tsx
+++ frontend/src/components/Filters/Filters.tsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 import styles from "./Filters.module.scss";
 
 type Props = {
@@ -9,23 +12,27 @@ type Props = {
 
 const Filters = ({ onCodeChange, onDateChange }: Props) => {
   const { t } = useTranslation();
-
   const [code, setCode] = useState("");
-  const [date, setDate] = useState("");
+  const [date, setDate] = useState<Date | null>(null);
+
+  const toYMD = (date: Date | null) => {
+
+    console.log("toYMD called with:", date);
+
+    if (!date) return "";
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+    return `${year}-${month}-${day}`;
+  };
 
   useEffect(() => {
-    const timeout = setTimeout(() => {
-      onCodeChange(code);
-    }, 500);
-
+    const timeout = setTimeout(() => onCodeChange(code), 500);
     return () => clearTimeout(timeout);
   }, [code, onCodeChange]);
 
   useEffect(() => {
-    const timeout = setTimeout(() => {
-      onDateChange(date);
-    }, 500);
-
+    const timeout = setTimeout(() => onDateChange(toYMD(date)), 500);
     return () => clearTimeout(timeout);
   }, [date, onDateChange]);
 
@@ -38,15 +45,16 @@ const Filters = ({ onCodeChange, onDateChange }: Props) => {
         onChange={(e) => setCode(e.target.value)}
         className={styles.input}
       />
-
-      <input
-        type="date"
-        value={date}
-        onChange={(e) => setDate(e.target.value)}
+      <DatePicker
+        selected={date}
+        placeholderText={t("searchByDate")}
+        onChange={(newDate) => setDate(newDate)}
+        dateFormat="dd-MM-yyyy"
         className={styles.input}
       />
     </div>
   );
 };
+
 
 export default Filters;
